577831014e2d9177f70a9c3226ae5a2e45aa2bb5
Nguyen Tien Dat <tie.dat2004@gmail.com>
2025-02-21 14:30:02 +0700

    next update

--- src/main/java/org/datCo/Main.java
+++ src/main/java/org/datCo/Main.java
@@ -1,17 +1,84 @@
 package org.datCo;
 
+import java.sql.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void getNoStatement(Connection con) throws SQLException {
+        String sql = "select * from app_food";
+
+        // Cung cap cac phuong thuc de thuc thi truy van
+        Statement statement = con.createStatement();
+
+        // Tra ve tap du lieu nhieu hang. Hoat dong nhu mot cursor tro den tung dong
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            int price = resultSet.getInt("price");
+
+            System.out.printf("%d - %s - %d\n", id, name, price);
         }
+
+    }
+
+
+    public static void insertWithStatement(Connection con) throws SQLException {
+        String sql = "insert into app_maincategory(name, active) values(?,?)";
+        PreparedStatement statement = con.prepareStatement(sql);
+        statement.setString(1, "Hủ tếu");
+        statement.setInt(2, 1);
+
+        int rows = statement.executeUpdate();
+        String notice = rows > 0 ? "Success":"Fail";
+        System.out.println(notice);
+
+    }
+
+    public static void getCallableStatement(Connection con) throws SQLException {
+//        CallableStatement callableStatement = con.prepareCall("{call get_products_by_kw(?)}");
+//        callableStatement.setString(1, "bun bo");
+
+        CallableStatement callableStatement = con.prepareCall("{ call count_orders_by_restaurant(?, ?) }");
+        callableStatement.setInt(1, 1);
+        callableStatement.registerOutParameter(2, java.sql.Types.INTEGER);
+
+        callableStatement.execute();
+
+        int rows = callableStatement.getInt(2);
+        System.out.println("Order quantity: "+rows);
+
+//        while (resultSet.next()) {
+//            int id = resultSet.getInt("id");
+//            String name = resultSet.getString("name");
+//            int price = resultSet.getInt("price");
+//
+//            System.out.printf("%d - %s - %d\n", id, name, price);
+//        }
+
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        // Nap Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Thiet lap ket noi
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/foodapp4db", "root", "tiendatmySQL964@");
+
+            getCallableStatement(connection);
+//            insertWithStatement(connection);
+//            getNoStatement(connection);
+
+
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
